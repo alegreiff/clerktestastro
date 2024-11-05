581897eeed6595da9a640c6095e8141bcf11c85b
Jaime de Greiff <alegreiff@gmail.com>
2024-11-05 15:24:08 -0500

    fly UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import clerk from '@clerk/astro'
 import vercel from '@astrojs/vercel/serverless';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [clerk(), tailwind()],
   output: 'server',
   adapter: vercel()
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,27 @@
-import { clerkMiddleware } from "@clerk/astro/server";
+//import { clerkMiddleware } from "@clerk/astro/server";
 
-export const onRequest = clerkMiddleware();
+//export const onRequest = clerkMiddleware();
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server'
+
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/public(.*)'])
+const isAdminRoute=createRouteMatcher(['/admin(.*)'])
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  
+  const { redirectToSignIn, userId, has } = auth()
+
+  if (!userId && isProtectedRoute(context.request)) {
+    // Add custom logic to run before redirecting
+
+    //return redirectToSignIn()
+    return context.redirect('/');
+  }
+
+
+  
+
+
+
+})
+
